09b06194ca77c3094aaa0123145d1d2c0277d16a
James Gayvert <jrg444@gmail.com>
2020-09-16 15:54:19 -0400

    Fixed duplicate case for power rankings

--- espn_api/football/power_rankings.py
+++ espn_api/football/power_rankings.py
@@ -48,18 +48,42 @@ def get_all_bench_points(league,week=17):
         team.bench_points = bench_points
     return sorted(league.teams,key=lambda x: x.bench_points, reverse=True)
 
+def assign_ranks(teams,ovr,points,bench,record,week):
+    for team in teams:
+        # ovr first
+        for i in range(0,len(ovr)):
+            if team.ovw == ovr[i].ovw:
+                team.ovr_rank = i+1
+                break
+        # now pts
+        for i in range(0,len(points)):
+            if np.sum(team.scores[:week]) == np.sum(points[i].scores[:week]):
+                team.pts_rank = i+1
+                break
+
+        # now bench
+        for i in range(0,len(bench)):
+            if team.bench_points == ovr[i].bench_points:
+                team.bnch_rank = i
+                break
+        # now record
+        for i in range(0,len(record)):
+            if team.wins/(team.wins+team.losses) == record[i].wins/(record[i].wins+record[i].losses):
+                team.record_rank = i+1
+                break
 
 def power_rankings(league,week):
     overall_records = get_all_overall_record(league,week=week)
     points_scored = get_all_points_scored(league,week)
     bench_points = get_all_bench_points(league,week)   
     records = sorted(league.teams,key=lambda x: x.wins/(x.wins+x.losses), reverse=True)
+    assign_ranks(league.teams,overall_records,points_scored,bench_points,records,week)
     for team in league.teams:
         score = 0
-        score += 0.3 * (overall_records.index(team)+1)
-        score += 0.3 * (points_scored.index(team)+1)
-        score += 0.3 * (records.index(team)+1)
-        score += 0.1 * (bench_points.index(team)+1)
+        score += 0.3 * team.ovr_rank
+        score += 0.3 * team.pts_rank
+        score += 0.3 * team.record_rank
+        score += 0.1 * team.bnch_rank
         team.power_score = score
         team.ovr_rank = overall_records.index(team)+1
         team.pts_rank = points_scored.index(team)+1
